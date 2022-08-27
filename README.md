7e2ee877399f0587b5014a6d3acbc5215bde0cbb
baba-s <shotababa1206@gmail.com>
2022-08-27 13:14:38 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.IDisposableAddToExtensionMethods
+# Kogane IDisposable Add To Extension Methods
+
+IDisposable の AddTo 拡張メソッド
---
+++ Runtime/IDisposableAddToExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kogane
+{
+    public static class IDisposableAddToExtensionMethods
+    {
+        public static T AddTo<T>( this T self, GameObject gameObject ) where T : IDisposable
+        {
+            if ( !gameObject.TryGetComponent<DestroyEventListener>( out var listener ) )
+            {
+                listener = gameObject.AddComponent<DestroyEventListener>();
+            }
+
+            listener.OnDestroyed += () => self.Dispose();
+
+            return self;
+        }
+
+        public static T AddTo<T>( this T self, Component component ) where T : IDisposable
+        {
+            return self.AddTo( component.gameObject );
+        }
+    }
+}
